641fadc3d2443f91d4f3ad4b15e34df1fee7e403
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-05-31 11:01:29 +0300

    add task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+// *****
+const list = document.querySelector('#ingredients');
+console.log(list);
+list.textContent = 'Ingredients';
+//***
+for (let i = 0; i < ingredients.length; i += 1) {
+    let elementList = document.createElement('li');
+    elementList.classList.add('item');
+    elementList.textContent = ingredients[i];
+    list.append(elementList);
+    console.log(elementList);
+}
+
+//***
+// const markup = ingredients
+//     .map(ingredient => `<li class="item">${ingredient}</li>`)
+//     .join('');
+// console.log(markup);
+// list.innerHTML = markup;
+//***
