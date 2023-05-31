ab4e5d9873e20101a7c84b3f0d297c98d42f6734
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-05-31 12:33:39 +0300

    add task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,30 @@
 const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+    {
+        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'White and Black Long Fur Cat',
+    },
+    {
+        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        alt: 'Group of Horses Running',
+    },
 ];
+
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+
+const image = images
+    .map(
+        img =>
+            `<li > <img class='img' width='350px', height='250px' src=${img.url}, alt =${img.alt}> </li>`
+    )
+    .join('');
+console.log(image);
+
+gallery.insertAdjacentHTML('afterbegin', image);
+gallery.style.display = 'flex';
+gallery.style.listStyleType = 'none';
+gallery.style.gap = '20px';
