7a99673341f451081d2312b42c84951cd551fab8
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-01 18:37:34 +0300

    add task-05

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const textInput = document.querySelector('#name-input');
+console.log(textInput);
+const output = document.querySelector('#name-output');
+console.log(output);
+textInput.addEventListener('input', event => {
+    output.textContent =
+        event.currentTarget.value === ''
+            ? 'Anonymis'
+            : event.currentTarget.value;
+});
