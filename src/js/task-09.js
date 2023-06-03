5d2e429c3f90826f23cd0ae39e492a71ed3850fc
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-03 13:41:28 +0300

    add task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector('.change-color');
+console.log(btnChangeColor);
+const color = document.querySelector('.color');
+btnChangeColor.addEventListener('click', () => {
+    color.style.backgroundColor = getRandomHexColor();
+});
