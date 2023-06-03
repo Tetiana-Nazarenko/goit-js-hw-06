ecbb84390b0ed9a28d21f8d71423c20646900039
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-03 13:17:54 +0300

    add task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,15 @@
+const submitButton = document.querySelector('.login-form');
+console.log(submitButton);
 
+submitButton.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === '' && password.value === '') {
+        return alert('Потрібно заповнити поле');
+    }
+    console.log(`Email:${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
