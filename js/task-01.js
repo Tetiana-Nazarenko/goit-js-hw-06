ee072c72911ab1a045610b6863f0c5e7b6e478b5
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-06 11:10:29 +0300

    update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const itemList = document.querySelectorAll('.item');
-console.log(`Number of categories: ${itemList.length}`);
+console.log('Number of categories:' + itemList.length);
 
 itemList.forEach(item => {
-    const title = item.querySelector('h2').textContent;
-    let oneTitle = item.querySelectorAll('li');
-    console.log(`Category: ${title}, Elements:${oneTitle.length}`);
+    const title = item.firstElementChild.textContent;
+    let oneTitle = item.lastElementChild.children;
+    console.log('Category:' + title, 'Elements:' + oneTitle.length);
 });
