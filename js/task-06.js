02a82e50b22d76a574cabab1f8d6ef2cb09503d5
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-06 11:36:06 +0300

    update task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,11 +2,13 @@ const inputText = document.querySelector('#validation-input');
 console.log(inputText.value.length);
 
 function handleControlSymbol() {
-    if (inputText.value.length === Number(inputText.dataset.length)) {
+    if (inputText.value.length == Number(inputText.dataset.length)) {
         console.log('true');
-        return inputText.classList.add('valid');
+        inputText.classList.add('valid');
+        inputText.classList.remove('invalid');
     } else {
         inputText.classList.add('invalid');
+        inputText.classList.remove('valid');
         console.log('false');
     }
 }
